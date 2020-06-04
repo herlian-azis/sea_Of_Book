6809aa011d24fe0758017683ebeeed218ce15db5
amiruljbr <amiruljabar@gmail.com>
2020-06-04 21:43:58 +0700

    ganti data json dan dummy

--- seeders/20200603081514-seeding-Books.js
+++ seeders/20200603081514-seeding-Books.js
@@ -1,6 +1,9 @@
 'use strict';
 const books = require('../files/books.json');
+const dirName = 'http://localhost:3000';
+
 books.forEach((book)=>{
+  book.path = dirName + '/books/downloads/' + book.path;
   book.createdAt = new Date();
   book.updatedAt = new Date();
 })
